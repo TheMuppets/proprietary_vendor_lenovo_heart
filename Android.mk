da0856fbf42e013f938707d2d553f8f8c42556ff
Michael Bestas <mkbestas@gmail.com>
2023-08-24 07:32:19 +0300

    heart: Add proprietary-firmware.txt
    Change-Id: Id78fbda480f86154ae981e6d67d8e21424e0409c

--- Android.mk
+++ Android.mk
@@ -3,3 +3,25 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),heart)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,300688681f0ddfedb431e64bdc1f447a8032de84)
+$(call add-radio-file-sha1-checked,radio/aop.img,a7c30b340c69571a62a47e5627d779d626a2907f)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,ae8785890bc552074a8dc2e704a9f61e9eb0e395)
+$(call add-radio-file-sha1-checked,radio/cmnlib.img,d6916875114c7bb0985a6fc0572cb03726aef764)
+$(call add-radio-file-sha1-checked,radio/cmnlib64.img,640eb767acca1d1af7a83a960dd41a9fc79907e0)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,b4ae2e3b3fdfe186d85823ac33ff70c9fc6ce8a3)
+$(call add-radio-file-sha1-checked,radio/dsp.img,cf810e477827f50dd2af0dd62ec250b03d392890)
+$(call add-radio-file-sha1-checked,radio/hyp.img,d8edcd9af21fddfa15310de9e862413c01ef2292)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,5846a2adbe206313897849a3e87f4b6a23c81606)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,3efae0d8ca0f5f101f9882d9aebf1e0ef3869b4b)
+$(call add-radio-file-sha1-checked,radio/modem.img,5f0be3695dcd87904cfa53c18fd01e20f2fd538e)
+$(call add-radio-file-sha1-checked,radio/multiimgoem.img,dbe81117e9bfa99851c55e8c5501e4889ff9d00b)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,188d57379261ec3d531dbf6c456198f5912c4cfa)
+$(call add-radio-file-sha1-checked,radio/storsec.img,92652d3dfc2548440094505271702dcf37b5c7e0)
+$(call add-radio-file-sha1-checked,radio/tz.img,66424a08be961cc7d0214b9d761cb1f3df09283d)
+$(call add-radio-file-sha1-checked,radio/xbl.img,3dda7531d3cf84b98a2fc866b8ef607ad06e4e0e)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,9e8b20193b103a37f548059d1f9e94387ae54b3c)
+
+endif
